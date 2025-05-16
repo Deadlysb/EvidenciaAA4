9dd2fbf20700890786c179a86f034f2ccd258a6d
Deadlysb <Faraday2706@gmail.com>
2025-05-16 01:36:38 -0500

    update 1.0

--- models/values.js
+++ models/values.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const valueSchema = new mongoose.Schema({
     label: {type:String, required: true},
     data: {type:Number, required: true}
+},{
+    timestamps: true
 });
 
 module.exports = mongoose.model('statistics', valueSchema,'values');
